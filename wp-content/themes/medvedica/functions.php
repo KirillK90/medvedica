4db0020f61b8855f0fc83726535edc6a84b1cd95
Kirill Kurkin <kirill.kurkin@adv-cake.ru>
2018-05-31 21:11:44 +0300

    fb_js

--- wp-content/themes/medvedica/functions.php
+++ wp-content/themes/medvedica/functions.php
@@ -26,6 +26,30 @@ HTML;
 
 add_action( 'presscore_body_top', 'add_vk_chat' );
 
+function add_fb_js() {
+    echo <<<HTML
+<!— Facebook Pixel Code —>
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '190245724962474');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=190245724962474&ev=PageView&noscript=1"
+/></noscript>
+<!— End Facebook Pixel Code —>
+HTML;
+}
+// Add hook for front-end <head></head>
+add_action('wp_head', 'add_fb_js');
+
 function remove_admin_bar_links() {
     /** @var $wp_admin_bar WP_Admin_Bar */
     global $wp_admin_bar;
